4e964f9b09c998f488e27cd28e7097caad796ef9
DegenPlays <DegenPlays_Dev@proton.me>
2023-05-31 15:56:41 -0300

    initial template

---
+++ components/pricing-tables.tsx
@@ -0,0 +1,182 @@
+'use client'
+
+import { useState } from 'react'
+
+export default function PricingTables() {
+
+  const [annual, setAnnual] = useState<boolean>(true)
+
+  return (
+    <section className="relative">
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12">
+            <h1 className="h1 mb-4" data-aos="fade-up">ICO Pricing</h1>
+            <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">Get DegenPlays at a discounted price for the first 500,000 DegenPlays sold.</p>
+          </div>
+
+          {/* Pricing tables */}
+          <div>
+
+            <div className="max-w-sm mx-auto grid gap-8 lg:grid-cols-3 lg:gap-6 items-start lg:max-w-none">
+
+              {/* Pricing table 1 */}
+              <div className="relative flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="700">
+                <div className="mb-4 pb-4 border-b border-gray-700">
+                  <div className="h4 text-purple-600 mb-1">1-1000 DegenPlays</div>
+                  <div className="inline-flex items-baseline mb-2">
+                    <span className="text-2xl md:text-3xl font-medium text-gray-400">$</span>
+                    <span className="h2">1</span>
+                    <span className="font-medium text-gray-400">/DegenPlays</span>
+                  </div>
+                  <div className="text-gray-400">Get in on degen plays with little investment.</div>
+                </div>
+                {/* <div className="font-medium mb-3">Features include:</div> */}
+                <ul className="text-gray-400 -mb-3 grow">
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Join in on the degen plays with as little as $1.</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Benefit from token value increases like everyone else.</span>
+                  </li>
+                  {/* <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Excepteur sint occaecat cupidatat</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Officia deserunt mollit anim</span>
+                  </li> */}
+                </ul>
+                <div className="border border-gray-700 p-3 mt-6">
+                  <a className="btn-sm text-white bg-purple-600 hover:bg-purple-700 w-full" href="#0">Buy DegenPlays</a>
+                </div>
+              </div>
+
+              {/* Pricing table 2 */}
+              <div className="relative flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="600">
+                {/* <div className="absolute top-0 right-0 mr-6 -mt-4">
+                  <div className="inline-flex text-sm font-semibold py-1 px-3 mt-px text-green-600 bg-green-200 rounded-full">Most Popular</div>
+                </div> */}
+                <div className="mb-4 pb-4 border-b border-gray-700">
+                  <div className="h4 text-purple-600 mb-1">1001-5000 DegenPLays</div>
+                  <div className="inline-flex items-baseline mb-2">
+                    <span className="text-2xl md:text-3xl font-medium text-gray-400">$</span>
+                    <span className="h2">0.90</span>
+                    <span className="font-medium text-gray-400">/DegenPlays</span>
+                  </div>
+                  <div className="text-gray-400">Save a bit with a larger investment.</div>
+                </div>
+                {/* <div className="font-medium mb-3">All features of Essential plus:</div> */}
+                <ul className="text-gray-400 -mb-3 grow">
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Get a few extra DegenPlays for showing your support.</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Consectetur adipiscing elit</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Excepteur sint occaecat cupidatat</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Officia deserunt mollit anim</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Placeholder text commonly used</span>
+                  </li>
+                </ul>
+                <div className="border border-gray-700 p-3 mt-6">
+                  <a className="btn-sm text-white bg-purple-600 hover:bg-purple-700 w-full" href="#0">Buy DegenPlays</a>
+                </div>
+              </div>
+
+              {/* Pricing table 3 */}
+              <div className="relative flex flex-col h-full p-6 bg-gray-800" data-aos="fade-up" data-aos-delay="800">
+                <div className="mb-4 pb-4 border-b border-gray-700">
+                  <div className="h4 text-purple-600 mb-1">5001+ DegenPLays</div>
+                  <div className="inline-flex items-baseline mb-2">
+                    <span className="text-2xl md:text-3xl font-medium text-gray-400">$</span>
+                    <span className="h2">0.85</span>
+                    <span className="font-medium text-gray-400">/DegenPlays</span>
+                  </div>
+                  <div className="text-gray-400">Better insights for growing businesses that want more customers.</div>
+                </div>
+                {/* <div className="font-medium mb-3">All features of Essential plus:</div> */}
+                <ul className="text-gray-400 -mb-3 grow">
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>200 placeholder text commonly</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Consectetur adipiscing elit</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Excepteur sint occaecat cupidatat</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Officia deserunt mollit anim</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Voluptate velit esse cillum</span>
+                  </li>
+                  <li className="flex items-center mb-3">
+                    <svg className="w-3 h-3 fill-current text-green-500 mr-3 shrink-0" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10.28 2.28L3.989 8.575 1.695 6.28A1 1 0 00.28 7.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 2.28z" />
+                    </svg>
+                    <span>Placeholder text commonly used</span>
+                  </li>
+                </ul>
+                <div className="border border-gray-700 p-3 mt-6">
+                  <a className="btn-sm text-white bg-purple-600 hover:bg-purple-700 w-full" href="#0">Buy DegenPlays</a>
+                </div>
+              </div>
+            </div>
+          </div>
+            <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">Once the intial 500,000 DegenPlays are sold the price will be increased to $1 + current token value until token value equals $5 then sales will be at current value.</p>
+        </div>
+            <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">Trades on any DEX with a DegenPlays pool will always be possible at current value. We will not be trying to influence the market. Prices here are for new token purchases.</p>
+      </div>
+    </section>
+  )
+}
---
+++ components/process.tsx
@@ -0,0 +1,59 @@
+export default function Process() {
+  return (
+    <section>
+      <div className="max-w-6xl mx-auto px-4 sm:px-6">
+        <div className="pt-10 pb-12 md:pt-16 md:pb-20">
+
+          {/* Section header */}
+          <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+            <h2 className="h2 mb-4" data-aos="fade-up">Join in on high rewarding Degen PLays without the High Risk</h2>
+            <p className="text-xl text-gray-400" data-aos="fade-up" data-aos-delay="200">With DegenPlays we handle all the managment of degen plays to help you earn the high rewards possible with less risk.</p>
+          </div>
+
+          {/* Items */}
+          <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-3 lg:gap-16 items-start md:max-w-none">
+
+            {/* 1st item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up">
+              <div aria-hidden="true" className="absolute h-1 border-t border-dashed border-gray-700 hidden md:block" style={{ width: 'calc(100% - 32px)', left: 'calc(50% + 48px)', top: '32px' }} data-aos="fade-in" data-aos-delay="200"></div>
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-300" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-100" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
+              </svg>
+              <h4 className="h4 mb-2"><span className="text-gray-400">1. </span>Buy DegenPlays</h4>
+              <p className="text-lg text-gray-400 text-center">Purchase DegenPlay tokens On any of our suporrted chains with any of our suported tokens</p>
+            </div>
+
+            {/* 2nd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="200">
+              <div aria-hidden="true" className="absolute h-1 border-t border-dashed border-gray-700 hidden md:block" style={{ width: 'calc(100% - 32px)', left: 'calc(50% + 48px)', top: '32px' }} data-aos="fade-in" data-aos-delay="400"></div>
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <g fill="none" fillRule="evenodd">
+                  <path className="stroke-current text-purple-300" d="M40 22a2 2 0 012 2v16a2 2 0 01-2 2H24a2 2 0 01-2-2V24a2 2 0 012-2" strokeWidth="2" strokeLinecap="square" />
+                  <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M36 32l-4-3-4 3V22h8z" />
+                </g>
+              </svg>
+              <h4 className="h4 mb-2"><span className="text-gray-400">2. </span>Watch your Tokens do the work</h4>
+              <p className="text-lg text-gray-400 text-center">Watch as the token price goes up as we make degen plays with your invested tokens.</p>
+            </div>
+
+            {/* 3rd item */}
+            <div className="relative flex flex-col items-center" data-aos="fade-up" data-aos-delay="400">
+              <svg className="w-16 h-16 mb-4" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <rect className="fill-current text-purple-600" width="64" height="64" rx="32" />
+                <path className="stroke-current text-purple-300" strokeWidth="2" strokeLinecap="square" d="M21 35l4 4 12-15" fill="none" fillRule="evenodd" />
+                <path className="stroke-current text-purple-100" d="M42 29h-3M42 34h-7M42 39H31" strokeWidth="2" strokeLinecap="square" />
+              </svg>
+              <h4 className="h4 mb-2"><span className="text-gray-400">3. </span>Cash out</h4>
+              <p className="text-lg text-gray-400 text-center">Sell your tokens back to us or trade on any exchange with a pair setup. There is no lock in period or trade limits.</p>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </section>
+  )
+}
