8409e37f44c9ab18163cacc3015fbf9181d0e1c8
DegenPlays <DegenPlays_Dev@proton.me>
2023-06-15 18:17:54 -0300

    fixed dash

--- app/(default)/dashboard/page.tsx
+++ app/(default)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 export const metadata = {
   title: 'Dashboard - DegenPlays',
   description: 'Page description',
@@ -10,20 +10,7 @@ import { useEffect, useState } from 'react';
 import {initializeWeb3Instances} from '@/components/utils/config'
 
 export default function Dashboard() {
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => { 
-    async function initializeWeb3() {
-    await initializeWeb3Instances();
-    setIsLoading(false);
-  }
-  initializeWeb3();
-  }, []);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-  
   return (
     <>
       <UserStats />
