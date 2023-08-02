ed0ee2c2ffface1ca3f7b66391c050e05cb30811
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2023-08-02 13:14:23 -0300

    ad update

--- components/ads.tsx
+++ components/ads.tsx
@@ -11,16 +11,7 @@ export default function Ads() {
         <Image src={QPad} alt="Clickable GIF" />
       </a>
       <div data-aos="fade-up" data-aos-delay="400">
-        <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://cryptoprogram.me?referralCode=7D659B50"  target="_blank" rel="noopener noreferrer">Crypto Program</a>
-      </div>
-      <div data-aos="fade-up" data-aos-delay="400">
-        <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://qliothon.com/#/?code=eDBaWt"  target="_blank" rel="noopener noreferrer">Qliothon</a>
-      </div>
-      <div data-aos="fade-up" data-aos-delay="400">
-        <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://alligatorcore.ai/register/144yXWkVMiMSE8"  target="_blank" rel="noopener noreferrer">aligator core</a>
-      </div>
-      <div>
-        <a href="https://poradex.com/ref/marsniper27" target="_blank"><img src="https://poradex.com/assets/banners/728.gif"/></a>
+        <a className="btn text-white bg-purple-600 hover:bg-purple-700 w-full mb-4 sm:w-auto sm:mb-0" href="https://amsys.io/"  target="_blank" rel="noopener noreferrer">AMSYS enter 7D659B50 to access form</a>
       </div>
     </div>
   );
