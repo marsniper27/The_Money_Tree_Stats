b2e8128ddb9200414ede5480d2bec61709055163
marsniper27 <kyle_marshall_sniper_27@hotmail.com>
2023-08-28 22:34:50 -0300

    Coming soon

--- app/(default)/howItWorks/page.tsx
+++ app/(default)/howItWorks/page.tsx
@@ -10,6 +10,14 @@ import FAQs from '@/components/faqs'
 export default function Features() {
   return (
     <>
+      {/* Hero content */}
+      <div className="relative pt-32 pb-10 md:pt-40 md:pb-16">
+
+        {/* Section header */}
+        <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
+          <p className="text-xl text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="200">Coming Soon</p>
+        </div>
+      </div>
       {/* <Hero /> */}
       {/* <Stats />
       <FAQs/> */}
